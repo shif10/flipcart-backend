3a7dec3ba41a4c7ac4b1de3b473e7bf5dd486ee3
shifa <shifab@lanatusystems.com>
2024-03-01 16:21:29 +0530

    url change

--- database/db.js
+++ database/db.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 export const Connection = async () => {
   console.log("hlo");
   try {
-    const URL = "mongodb://127.0.0.1/flipcart-clone";
+    // const URL = "mongodb://127.0.0.1/flipcart-clone";
 
-    // const URL =
-    //   process.env.MONGODBURL ||
-    //   `mongodb+srv://${process.env.user}:${process.env.password}@cluster0.xwg166w.mongodb.net/?retryWrites=true&w=majority`;
+    const URL =
+      process.env.MONGODBURL ||
+      `mongodb+srv://${process.env.user}:${process.env.password}@cluster0.xwg166w.mongodb.net/?retryWrites=true&w=majority`;
     await mongoose.connect(URL, {
       // useNewUrlParser: true,
     });
